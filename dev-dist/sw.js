78f0be931efe78aa50199ee9f46b56cdd3cc78bd
sachinimaginet <sachin@imaginetventures.com>
2025-12-17 00:17:13 +0530

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8a0jumd73mo"
+    "revision": "0.g04dcmspoog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
